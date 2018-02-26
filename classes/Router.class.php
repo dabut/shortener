d354cd8f1739dedd7a4c7bf3c5394d0fa7992061
dabut <dylanbutler@outlook.com.au>
2018-02-26 11:17:01 +1100

    router parses routes

--- classes/Router.class.php
+++ classes/Router.class.php
@@ -2,22 +2,45 @@
 
 	class Router {
 
-		private $page;
+		private $request = 'home';
 
 		public function parseGet($get) {
+
 			if (isset($get['request'])) {
-				$this->page = $this->parseRequest($get['request']);
+
+				$this->request = $this->parseRequest($get['request']);
+
 			} else {
-				$this->page = 'home';
+
+				$this->request = 'home';
 			}
 		}
 
 		public function parseRequest($request) {
 
+			$requestParts = explode('/', $request);
 
+			if (count($requestParts) > 0) {
+				
+				if ($requestParts[0] == '') {
 
+					unset($requestParts[0]);
+				}
+
+				if ($requestParts[count($requestParts) - 1] == '') {
+
+					unset($requestParts[count($requestParts) - 1]);
+				}
+
+				$requet = implode('/', $requestParts);
+
+				$this->request = $request;
+
+			} else {
+
+				throw new Exception("Error parsing request");
+			}
 		}
-
 	}
 
 ?>
